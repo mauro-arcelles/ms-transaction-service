19035adaf9c75b177af7e4eb7f70777901b2479e
mauro-arcelles <mauroar1001@hotmail.com>
2025-02-11 12:22:01 -0500

    feature: use service base url from config server

--- src/main/java/com/project1/ms_transaction_service/WebClientConfig.java
+++ src/main/java/com/project1/ms_transaction_service/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.project1.ms_transaction_service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,13 +8,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${account.service.baseUrl}")
+    private String accountServiceBaseUrl;
+
+    @Value("${credit.service.baseUrl}")
+    private String creditServiceBaseUrl;
+
     @Bean("accountWebClient")
     public WebClient accountClient() {
-        return WebClient.create("http://localhost:8091/api/v1");
+        return WebClient.builder()
+                .baseUrl(accountServiceBaseUrl)
+                .build();
     }
 
     @Bean("creditCardWebClient")
     public WebClient creditCardClient() {
-        return WebClient.create("http://localhost:8093/api/v1");
+        return WebClient.builder()
+                .baseUrl(creditServiceBaseUrl)
+                .build();
     }
 }
