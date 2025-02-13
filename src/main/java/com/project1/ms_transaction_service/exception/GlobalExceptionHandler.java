82261243dff5a1b69410a3d5746e7c7bde9271b8
mauro-arcelles <mauroar1001@hotmail.com>
2025-02-13 09:38:38 -0500

    feature: show exception for invalid given types in request body

--- src/main/java/com/project1/ms_transaction_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project1/ms_transaction_service/exception/GlobalExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.project1.ms_transaction_service.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.project1.ms_transaction_service.model.ResponseBase;
+import org.springframework.core.codec.DecodingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -49,5 +53,22 @@ public class GlobalExceptionHandler {
         responseBase.setMessage(ex.getMessage());
         return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(responseBase));
+    }
+
+    @ExceptionHandler(ServerWebInputException.class)
+    public Mono<ResponseEntity<Map<String, List<String>>>> handleServerWebInputException(ServerWebInputException ex) {
+        Map<String, List<String>> errors = new HashMap<>();
+
+        if (ex.getCause() instanceof DecodingException) {
+            DecodingException decodingException = (DecodingException) ex.getCause();
+            if (decodingException.getCause() instanceof InvalidFormatException) {
+                InvalidFormatException invalidFormatException = (InvalidFormatException) decodingException.getCause();
+                String fieldName = invalidFormatException.getPath().get(0).getFieldName();
+                String targetType = invalidFormatException.getTargetType().getSimpleName();
+                errors.put(fieldName, List.of("Must be a valid " + targetType.toLowerCase()));
+            }
+        }
+
+        return Mono.just(ResponseEntity.badRequest().body(errors));
     }
 }
